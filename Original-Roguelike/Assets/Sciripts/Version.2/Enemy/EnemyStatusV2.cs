d150e1cb14f409639612de6694e48b42b4d45768
ShunMiya <krnmeradrs@gmail.com>
2023-08-21 15:35:11 +0900

    Create : Enemy TakeDamage & EnemyDeathEvent

--- Original-Roguelike/Assets/Sciripts/Version.2/Enemy/EnemyStatusV2.cs
+++ Original-Roguelike/Assets/Sciripts/Version.2/Enemy/EnemyStatusV2.cs
@@ -1,3 +1,4 @@
+using Enemy;
 using System.Collections;
 using System.Collections.Generic;
 using UISystemV2;
@@ -7,8 +8,8 @@ namespace EnemySystem
 {
     public class EnemyStatusV2 : MonoBehaviour
     {
-        public delegate void EnemyDefeatedEventHandler();
-        public event EnemyDefeatedEventHandler EnemyDefeated;
+        public delegate void EnemyDeathEvent();
+        public event EnemyDeathEvent EnemyDeath;
         private SystemTextV2 systemText;
 
         public float currentHP;
@@ -17,15 +18,18 @@ namespace EnemySystem
         private void Start()
         {
             systemText = FindObjectOfType<SystemTextV2>();
+            EnemyDataV2 enemy = EnemyDataCacheV2.GetEnemyData(EnemyID);
+            currentHP = enemy.MaxHP;
+
         }
         public void TakeDamage(float damage)
         {
             currentHP -= damage;
             systemText.TextSet("Enemy" + damage + "Damage! HP:" + currentHP);
 
-            if (currentHP <= 0 && EnemyDefeated != null)
+            if (currentHP <= 0 && EnemyDeath != null)
             {
-                EnemyDefeated();
+                EnemyDeath();
             }
         }
 
