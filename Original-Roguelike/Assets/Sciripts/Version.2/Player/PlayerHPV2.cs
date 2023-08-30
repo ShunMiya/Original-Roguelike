582658932132e90f51699f796568e457fadb6643
ShunMiya <krnmeradrs@gmail.com>
2023-08-30 17:52:29 +0900

    Create : TurnRecoveryHP

--- Original-Roguelike/Assets/Sciripts/Version.2/Player/PlayerHPV2.cs
+++ Original-Roguelike/Assets/Sciripts/Version.2/Player/PlayerHPV2.cs
@@ -11,6 +11,7 @@ namespace PlayerStatusSystemV2
         private SqliteDatabase sqlDB;
         private SystemTextV2 systemText;
         private GameEndV2 gameEnd;
+        private float Recovery = 0;
 
         private void Start()
         {
@@ -81,6 +82,35 @@ namespace PlayerStatusSystemV2
             systemText.TextSet("Player" + Heal + "Heal! HP:" + HealHP);
 
             return true;
+        }
+
+        public void TurnRecoveryHp()
+        {
+            if (sqlDB == null)
+            {
+                string databasePath = SQLDBInitializationV2.GetDatabasePath();
+                sqlDB = new SqliteDatabase(databasePath);
+            }
+            if (systemText == null) systemText = FindObjectOfType<SystemTextV2>();
+            string query = "SELECT * FROM PlayerStatus WHERE PlayerID = 1;";
+            DataTable Data = sqlDB.ExecuteQuery(query);
+
+            if (Convert.ToInt32(Data[0]["CurrentHungry"]) == 0) return;
+
+            int CurrentHP = Convert.ToInt32(Data[0]["CurrentHP"]);
+            int MaxHP = Convert.ToInt32(Data[0]["MaxHP"]);
+
+            if (CurrentHP < MaxHP)
+            {
+                Recovery++;
+                if (Recovery >= 2)
+                {
+                    string updateStatusQuery = "UPDATE PlayerStatus SET CurrentHP = (SELECT CurrentHP FROM PlayerStatus WHERE PlayerID = 1) + " + 1 + " WHERE PlayerID = 1;";
+                    sqlDB.ExecuteNonQuery(updateStatusQuery);
+                    Recovery = 0;
+                }
+
+            }
         }
     }
 }
