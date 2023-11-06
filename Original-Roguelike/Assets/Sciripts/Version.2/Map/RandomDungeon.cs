6efa577ade697c20d1808eb920c9012f86750290
ShunMiya <krnmeradrs@gmail.com>
2023-11-06 15:20:51 +0900

    Edit :  Fix  PRCount  adjustment

--- Original-Roguelike/Assets/Sciripts/Version.2/Map/RandomDungeon.cs
+++ Original-Roguelike/Assets/Sciripts/Version.2/Map/RandomDungeon.cs
@@ -15,6 +15,8 @@ namespace Field
         private Array2D data;
         private List<Area2D> areas;
         private int Proom;
+        private int PRCount = 0;
+
 
 
         /**
@@ -22,6 +24,8 @@ namespace Field
         */
         public Array2D Create(int w, int h, Areamap field)
         {
+            PRCount = 0;
+
             data = new Array2D(w + 4, h + 4); //ŠO˜g‚Qƒ}ƒX‚Å•Ç‚ğ¶¬‚·‚é
             for (int x = 0; x < data.width; x++)
             {
@@ -252,18 +256,14 @@ namespace Field
 
         private void SetObject(string name, string type, Areamap field, Array2D data)
         {
-            int PCount = 0;
             while (true)
             {
                 int areaIdx = Random.Range(0, areas.Count);
 
-                if(type == "Enemy")
+                if(type == "Enemy" && areaIdx == Proom)
                 {
-                    if (areaIdx == Proom)
-                    {
-                        if (PCount >= 3) continue;
-                        PCount++;
-                    }
+                    if (PRCount >= 3) continue;
+                    PRCount++;
                 }
 
                 Rect2D room = areas[areaIdx].room;
