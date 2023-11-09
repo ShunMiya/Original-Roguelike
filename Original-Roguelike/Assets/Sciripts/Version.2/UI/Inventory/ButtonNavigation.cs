1c799f450608c0afa99fb0c73f501f08c8bed52c
ShunMiya <krnmeradrs@gmail.com>
2023-11-09 10:58:53 +0900

    Edit : Inventory-EquipWindow  ButtonNavigation Setting

--- Original-Roguelike/Assets/Sciripts/Version.2/UI/Inventory/ButtonNavigation.cs
+++ Original-Roguelike/Assets/Sciripts/Version.2/UI/Inventory/ButtonNavigation.cs
@@ -47,6 +47,15 @@ namespace UISystemV2
             SetItemButtonNavigation(ItemButton1, 1);
         }
 
+        public void SetEquipMenuButtonNavigation()
+        {
+            ItemButton0 = Content.transform.GetChild(0).gameObject.GetComponent<Button>();
+
+            SetMenuButtonNavigation(MenuButton2, ItemButton0, 2);
+
+            SetEquipButtonNavigation();
+        }
+
         public void SetMenuButtonNavigation(Button button, Button areabutton, int i)
         {
             Navigation newNavi = new Navigation();
@@ -83,7 +92,9 @@ namespace UISystemV2
                     newNavi.selectOnUp = MenuButton0.GetComponent<Selectable>();
                     newNavi.selectOnRight = ItemButton1.GetComponent<Selectable>();
 
-                    if(Content.transform.childCount > 2) newNavi.selectOnDown = Content.transform.GetChild(2).gameObject.GetComponent<Button>();
+                    newNavi.selectOnDown = Content.transform.childCount > 2
+                           ? Content.transform.GetChild(2).gameObject.GetComponent<Button>() : null;
+                    //if(Content.transform.childCount > 2) newNavi.selectOnDown = Content.transform.GetChild(2).gameObject.GetComponent<Button>();
                     break;
                 case 1:
                     newNavi.selectOnUp = MenuButton2.GetComponent<Selectable>();
@@ -102,6 +113,28 @@ namespace UISystemV2
             button.navigation = newNavi;
         }
 
+        public void SetEquipButtonNavigation()
+        {
+            for(int i=0; i < Content.transform.childCount; i++)
+            {
+                Button EquipButton = Content.transform.GetChild(i).gameObject.GetComponent<Button>();
+
+                Navigation newNavi = new Navigation();
+                newNavi.mode = Navigation.Mode.Explicit;
+                
+                
+                newNavi.selectOnUp = i > 0
+                    ? Content.transform.GetChild(i-1).gameObject.GetComponent<Selectable>()
+                    : MenuButton2.GetComponent<Selectable>();
+                
+                newNavi.selectOnDown = Content.transform.childCount > i+1
+                    ? Content.transform.GetChild(i+1).gameObject.GetComponent<Selectable>() : null;
+
+                newNavi.selectOnLeft = MenuButton0.GetComponent<Selectable>();
+
+                EquipButton.navigation = newNavi;
+            }
+        }
 
         public void ResetNavigation()
         {
